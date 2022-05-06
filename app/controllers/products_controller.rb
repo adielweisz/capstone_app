22c1697bbf767d1e858fef570fc8083d372b2441
Adele Weisz <adiel.weisz@gmail.com>
2022-05-06 16:31:54 -0500

    fixed seeds error need to finish controller and routes files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,50 @@
 class ProductsController < ApplicationController
+    def index
+      products = Product.all
+      render json: products.as_json
+    end
+  
+    def create
+      product = Product.new(
+        name: params[:name],
+        brand: params[:brand],
+        description: params[:description],
+        available_at: params[:available_at],
+        department: params[:department],
+        catagory: params[:catagory],
+        sub_catagory: params[:sub_catagory],
+        key_word: params [:key_word],
+        tutorial_id: params [:tutorial_id],        
+      )
+      product.save
+      render json: product.as_json
+    end
+  
+    def show
+      product = Product.find_by(id: params[:id])
+      render json: product.as_json
+    end
+  
+    def update
+      product = Product.find_by(id: params[:id])
+      product.name = params[:name] || product.name
+      product.brand = params[:brand] || product.brand
+      product.description = params[:description] || product.description
+      product.available_at = params[:available_at] || product.available_at
+      product.brand = params[:brand] || product.brand
+      product.catagory = params[:catagory] || product.catagory
+      product.sub_catagory = params[:sub_catagory] || product.sub_catagory
+      product.key_word = params[:key_word] || product.key_word
+      product.tutorial_id = params[:tutorial_id] || product.tutorial_id
+      product.product_id = params[:product_id] || product.product_id
+      product.save
+      render json: product.as_json
+    end
+  
+    def destroy
+      product = Product.find_by(id: params[:id])
+      product.destroy
+      render json: {message: "Product successfully destroyed."}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   Rails.application.routes.draw do
+    #Forms 
     get "/forms" => "forms#index"
     post "/forms" => "forms#create"
     get "/forms/:id" => "forms#show"
     patch "/forms/:id" => "forms#update"
     delete "/forms/:id" => "forms#destroy"
+
+    #Products
+    get "/products" => "products#index"
+    post "/products" => "products#create"
+    get "/products/:id" => "products#show"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
+
+
   end
 end
