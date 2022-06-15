9739f0cecd5f90398c9eaf3ac8911830f173f87a
Adele Weisz <adiel.weisz@gmail.com>
2022-06-15 10:27:24 -0500

    trying to fix an api bug and installed http gemfile

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem "rails", "~> 7.0.2", ">= 7.0.2.3"
 
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
-
+gem "http"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 gem 'active_model_serializers', '~> 0.10.0'
--- app/controllers/makeup_api_controller.rb
+++ app/controllers/makeup_api_controller.rb
@@ -1,6 +1,6 @@
 class MakeupApiController < ApplicationController
 def index
-  makeup = HTTP.get("http://makeup-api.herokuapp.com/api/v1/products.json")
+   makeup = HTTP.get("http://makeup-api.herokuapp.com/api/v1/products.json")
   render json: makeup.parse(:json)
 end 
 end
